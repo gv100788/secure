fa572ff7ee9a71f5d8444d66118f2eede9af2e97
gv100788 <ardi4all@gmail.com>
2018-06-08 12:25:26 +0700

    Update securelink_utama.js

--- securelink_utama.js
+++ securelink_utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="choosing-ideal-insolvency-practitioner.html"}
 if(x=="2"){xxx="types-of-insurance.html"}
 if(x=="3"){xxx="simple-ways-of-getting-winning.html"}
 if(x=="4"){xxx="monetary-policy.html"}
-if(x=="5"){xxx="ocean-harbor-insurance-complete.html"}a_to_ve[i].href="https://www.secure2link.ml/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="ocean-harbor-insurance-complete.html"}a_to_ve[i].href="https://www.tunggu.xyz/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
